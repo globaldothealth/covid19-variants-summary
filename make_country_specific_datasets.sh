9a3c6ab06d4dbb700dc26e3648a7a1d555750bfc
Abhishek Dasgupta <abhishek.dasgupta@cs.ox.ac.uk>
2022-03-21 09:18:08 +0000

    Call python3 instead of python in build

--- make_country_specific_datasets.sh
+++ make_country_specific_datasets.sh
@@ -2,12 +2,11 @@
 # Put country specific datasets in output/country
 # depends: jq
 set -exou pipefail
-
 COUNTRIES="$(jq -r '.[]' data/countries.json | tr '\n' ' ')"
 for c in $COUNTRIES; do
-    python analysis.py --country=$c
-    python analysis.py --country=$c --age
-    python analysis.py --country=$c --age --group
-    python analysis.py --country=$c --gender
-    python analysis.py --country=$c --gender --group
+    python3 analysis.py --country=$c
+    python3 analysis.py --country=$c --age
+    python3 analysis.py --country=$c --age --group
+    python3 analysis.py --country=$c --gender
+    python3 analysis.py --country=$c --gender --group
 done
